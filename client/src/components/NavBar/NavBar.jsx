5f4689c07fbfcac30aac95cc266a1ad678fc190d
Axel Valiente <axelvalientehex97@gmail.com>
2023-07-10 12:32:46 -0300

    app routing done

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import searchSrc from "../../assets/magnifying-glass.png";
+
 const NavBar = () => {
-  return <h1>NavBar</h1>;
+  return (
+    <header className="header">
+      <Link className="header__logo" to="/" />
+      <div className="header__div">
+        <input
+          className="header__div__input"
+          placeholder="Nunca dejes de buscar"
+          type="text"
+        />
+        <button className="header__div__button">
+          <img
+            className="header__div__button__image"
+            src={searchSrc}
+            alt="Lupa"
+          />
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,40 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./index.css";
+import NavBar from "./components/NavBar/Navbar.jsx";
+import Cards from "./components/Cards/Cards";
+import CardDetail from "./components/CardDetail/CardDetail";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <NavBar />
+        <Outlet />
+      </>
+    ),
+  },
+  {
+    path: "/items",
+    element: (
+      <>
+        <NavBar />
+        <Cards />
+      </>
+    ),
+  },
+  {
+    path: "/items/:id",
+    element: (
+      <>
+        <NavBar />
+        <CardDetail />
+      </>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
